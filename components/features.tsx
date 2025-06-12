8bc599c61c1176fee1afb2777a8b54d2d28bfb21
“sayan <sayansenapati2544@gmail.com>
2025-06-12 07:37:56 +0530

    updating features

--- components/features.tsx
+++ components/features.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react/no-unescaped-entities */
 import React from "react";
 
 function Features() {
@@ -8,80 +9,220 @@ function Features() {
           {/* Feature 1 */}
           <div>
             <div className="relative flex items-center justify-center mx-auto">
-              <svg className="text-blue-100 dark:text-blue-900" width="72" height="75" viewBox="0 0 72 75" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="text-blue-100 dark:text-blue-900"
+                width="72"
+                height="75"
+                viewBox="0 0 72 75"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M63.6911 28.8569C68.0911 48.8121 74.6037 61.2674 53.2349 65.9792C31.8661 70.6909 11.6224 61.2632 7.22232 41.308C2.82229 21.3528 3.6607 12.3967 25.0295 7.68503C46.3982 2.97331 59.2911 8.90171 63.6911 28.8569Z" />
               </svg>
-              <svg className="absolute text-blue-600 dark:text-blue-400 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4" />
+              <svg
+                className="absolute text-blue-600 dark:text-blue-400 w-9 h-9"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M12 11c0 3.517-1.009 6.799-2.753 9.571m-3.44-2.04l.054-.09A13.916 13.916 0 008 11a4 4 0 118 0c0 1.017-.07 2.019-.203 3m-2.118 6.844A21.88 21.88 0 0015.171 17m3.839 1.132c.645-2.266.99-4.659.99-7.132A8 8 0 008 4.07M3 15.364c.64-1.319 1-2.8 1-4.364 0-1.457.39-2.823 1.07-4"
+                />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">Secured Payments</h3>
-            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">
+              Secured Payments
+            </h3>
+            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
           </div>
           {/* Feature 2 */}
           <div>
             <div className="relative flex items-center justify-center mx-auto">
-              <svg className="text-orange-100 dark:text-orange-900" width="62" height="64" viewBox="0 0 62 64" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="text-orange-100 dark:text-orange-900"
+                width="62"
+                height="64"
+                viewBox="0 0 62 64"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M62 13.001C62 33.4355 53.9345 64.001 33.5 64.001C13.0655 64.001 0 50.435 0 30.0005C0 9.56596 2.56546 4.00021 23 4.00021C43.4345 4.00021 62 -7.43358 62 13.001Z" />
               </svg>
-              <svg className="absolute text-orange-600 dark:text-orange-400 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M13 10V3L4 14h7v7l9-11h-7z" />
+              <svg
+                className="absolute text-orange-600 dark:text-orange-400 w-9 h-9"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M13 10V3L4 14h7v7l9-11h-7z"
+                />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">Fast & Easy to Load</h3>
-            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">
+              Fast & Easy to Load
+            </h3>
+            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
           </div>
           {/* Feature 3 */}
           <div>
             <div className="relative flex items-center justify-center mx-auto">
-              <svg className="text-green-100 dark:text-green-900" width="66" height="68" viewBox="0 0 66 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="text-green-100 dark:text-green-900"
+                width="66"
+                height="68"
+                viewBox="0 0 66 68"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M65.5 30C65.5 50.4345 46.4345 68 26 68C5.56546 68 0 50.4345 0 30C0 9.56546 12.5655 0 33 0C53.4345 0 65.5 9.56546 65.5 30Z" />
               </svg>
-              <svg className="absolute text-green-600 dark:text-green-400 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+              <svg
+                className="absolute text-green-600 dark:text-green-400 w-9 h-9"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">Light & Dark Version</h3>
-            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">
+              Light & Dark Version
+            </h3>
+            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
           </div>
           {/* Feature 4 */}
           <div>
             <div className="relative flex items-center justify-center mx-auto">
-              <svg className="text-purple-100 dark:text-purple-900" width="66" height="68" viewBox="0 0 66 68" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="text-purple-100 dark:text-purple-900"
+                width="66"
+                height="68"
+                viewBox="0 0 66 68"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M65.5 30C65.5 50.4345 46.4345 68 26 68C5.56546 68 0 50.4345 0 30C0 9.56546 12.5655 0 33 0C53.4345 0 65.5 9.56546 65.5 30Z" />
               </svg>
-              <svg className="absolute text-purple-600 dark:text-purple-400 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z" />
+              <svg
+                className="absolute text-purple-600 dark:text-purple-400 w-9 h-9"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M20.354 15.354A9 9 0 018.646 3.646 9.003 9.003 0 0012 21a9.003 9.003 0 008.354-5.646z"
+                />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">Flexible UI</h3>
-            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint. Velit officia consequat duis enim velit mollit.</p>
+            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">
+              Flexible UI
+            </h3>
+            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+              Amet minim mollit non deserunt ullamco est sit aliqua dolor do
+              amet sint. Velit officia consequat duis enim velit mollit.
+            </p>
           </div>
           {/* Feature 5 */}
           <div>
             <div className="relative flex items-center justify-center mx-auto">
-              <svg className="text-gray-100 dark:text-gray-900" width="65" height="70" viewBox="0 0 65 70" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="text-gray-100 dark:text-gray-900"
+                width="65"
+                height="70"
+                viewBox="0 0 65 70"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M64.5 26C64.5 46.4345 56.4345 70 36 70C15.5655 70 0 53.9345 0 33.5C0 13.0655 13.0655 0 33.5 0C53.9345 0 64.5 5.56546 64.5 26Z" />
               </svg>
-              <svg className="absolute text-gray-600 dark:text-gray-400 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z" />
+              <svg
+                className="absolute text-gray-600 dark:text-gray-400 w-9 h-9"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M9 12l2 2 4-4m5.618-4.016A11.955 11.955 0 0112 2.944a11.955 11.955 0 01-8.618 3.04A12.02 12.02 0 003 9c0 5.591 3.824 10.29 9 11.622 5.176-1.332 9-6.03 9-11.622 0-1.042-.133-2.052-.382-3.016z"
+                />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">Best Price Guarantee</h3>
-            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">We ensure you get the best rates for your stay. Find a lower price elsewhere within 24 hours of booking, and we'll match it plus give you an additional 10% off.</p>
+            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">
+              Best Price Guarantee
+            </h3>
+            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+              We ensure you get the best rates for your stay. Find a lower price
+              elsewhere within 24 hours of booking, and we'll match it plus give
+              you an additional 10% off.
+            </p>
           </div>
           {/* Feature 6 */}
           <div>
             <div className="relative flex items-center justify-center mx-auto">
-              <svg className="text-yellow-100 dark:text-yellow-900" width="78" height="78" viewBox="0 0 78 78" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+              <svg
+                className="text-yellow-100 dark:text-yellow-900"
+                width="78"
+                height="78"
+                viewBox="0 0 78 78"
+                fill="currentColor"
+                xmlns="http://www.w3.org/2000/svg"
+              >
                 <path d="M8.49996 28.0002C4.09993 47.9554 14.1313 66.7885 35.5 71.5002C56.8688 76.2119 68.0999 58.4553 72.5 38.5001C76.9 18.5449 68.3688 12.711 47 7.99931C25.6312 3.28759 12.9 8.04499 8.49996 28.0002Z" />
               </svg>
-              <svg className="absolute text-yellow-500 dark:text-yellow-400 w-9 h-9" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-                <path strokeLinecap="round" strokeLinejoin="round" strokeWidth="1.5" d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z" />
+              <svg
+                className="absolute text-yellow-500 dark:text-yellow-400 w-9 h-9"
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+              >
+                <path
+                  strokeLinecap="round"
+                  strokeLinejoin="round"
+                  strokeWidth="1.5"
+                  d="M8 7V3m8 4V3m-9 8h10M5 21h14a2 2 0 002-2V7a2 2 0 00-2-2H5a2 2 0 00-2 2v12a2 2 0 002 2z"
+                />
               </svg>
             </div>
-            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">Free Cancellation</h3>
-            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">Flexible booking options with free cancellation on most rooms. Change your travel dates or cancel your booking up to 24 hours before check-in without any charges.</p>
+            <h3 className="mt-8 text-lg font-semibold text-black dark:text-white">
+              Free Cancellation
+            </h3>
+            <p className="mt-4 text-base text-gray-600 dark:text-gray-300">
+              Flexible booking options with free cancellation on most rooms.
+              Change your travel dates or cancel your booking up to 24 hours
+              before check-in without any charges.
+            </p>
           </div>
         </div>
       </div>
